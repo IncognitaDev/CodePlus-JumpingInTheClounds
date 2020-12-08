6e19a2b23f7f9d966e2caf3a41988f2087bf6ad0
Luis Sousa <lv-ss@hotmail.com>
2020-12-07 23:05:15 -0300

    finalizando resolução

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const cenario1 = [0, 0, 1, 0, 0, 1, 0];
 const cenario2 = [0, 0, 0, 0, 1, 0];
 
-const jumpingOnClouds = c => {
+const jumpingOnClouds = (c) => {
+  var aux = false;
+  const jumps = c.reduce((acc, cloud, index, arr) => {
+    if (index == 0 || aux == true || cloud == 1) {
+      aux = false;
+      return acc;
+    }
+    if (cloud == 0) {
+      arr[index + 1] == 0 && arr[index - 1] != 1 ? (aux = true) : (aux = false);
 
-}
+      return (acc += 1);
+    }
+  }, 0);
 
-jumpingOnClouds(cenario1)
+  return jumps;
+};
+
+console.log(jumpingOnClouds(cenario1));
 
 //c representa o cenario, mantenha o nome pra eu poder testar no hackerrank :D
